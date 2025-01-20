550b7c7504812e82897ea559a4e8b41f74d9f5c2
TheWatcher01 <7450@holbertonstudents.com>
2025-01-20 11:09:55 +0100

    Simplify node deletion logic in delete_dnodeint_at_index function

--- challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -26,18 +26,16 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	if (current == NULL)
 		return (-1);
 
+	if (current->prev != NULL)
+		current->prev->next = current->next;
+	if (current->next != NULL)
+		current->next->prev = current->prev;
+
 	if (current == *head)
 	{
 		*head = current->next;
 		if (*head != NULL)
 			(*head)->prev = NULL;
-	}
-	else
-	{
-		if (current->prev != NULL)
-			current->prev->next = current->next;
-		if (current->next != NULL)
-			current->next->prev = current->prev;
 	}
 
 	free(current);
